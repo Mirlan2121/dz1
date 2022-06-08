1e8d91135ea3bccfc4cda5d98aace1333682c094
Mirlan <email>
2022-06-08 21:07:15 +0600

    dz 1

---
+++ index.js
@@ -0,0 +1,40 @@
+// const name = prompt('ваше имя',);
+// const age = prompt('ваше age',);
+
+// function myInfo(name, age){
+//     if(name && age !== '' || null){
+//         console.log(`Меня зовут ${name}. Мне ${age}`)
+//     }else {
+//         console.log('сори , не заполнили поле')
+//     }
+// }
+
+// myInfo(name, age)
+
+// const arrayNumber = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5,6,7,8,9,10]
+
+
+// const sayNumber = (number) => {
+
+//     let ok = number.filter(
+//         function(num){
+//         return num%2 === 0;
+//     }
+//     )
+
+//     console.log(ok)
+// }
+
+// sayNumber(arrayNumber)
+
+
+// const array = [1,2,45,"hello", 233, 100, "world", 45];
+
+// for (let i = 0; i < array.length; i++){
+//     console.log(typeof array[i]);
+//     if(typeof array[i] === 'number'){
+//         console.log(array[i]);
+//     }
+// }
+
+
